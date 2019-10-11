5698e3758150cb025cad02618a624d05e28050df
RiinaMiichelle <sarina.m.colosimo@gmail.com>
2019-10-10 20:29:34 -0500

    started towers game

---
+++ src/towers/towers.js
@@ -0,0 +1,10 @@
+
+    let $block = null;
+    
+    $('[data-row]').click(function() {
+      if ($block) {
+        $(this).append($block);
+        $block = null;
+      } else {
+        $block = $(this).children().last().detach();
+      }
